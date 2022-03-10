8d1ac4e8405da4555947d5f85936c8c6e98577ac
Vaibhav <Vaibhavshrotriyas@gmail.com>
2022-03-10 13:17:25 +0530

    Add files via upload

---
+++ singleStar.py
@@ -0,0 +1,7 @@
+a=int(input("Enter no. of row"))
+i=1
+
+while(i<=a):
+	print("*"*i, end='')
+	i=i+1
+	print()
---
+++ star.py
@@ -0,0 +1,10 @@
+a=int(input("Enter no. of row"))
+i=1
+
+while(i<=a):
+	j=1
+	while(j<=i):
+		print("*", end='')
+		j+=1
+	print()
+	i+=1
